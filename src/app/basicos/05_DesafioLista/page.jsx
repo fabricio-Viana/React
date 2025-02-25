16bc56c1e87db55b97e2c6a0ace968ff556c5bf0
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-02-25 16:01:45 -0300

    Desafio criandouma lista com JSX

---
+++ src/app/basicos/05_DesafioLista/page.jsx
@@ -0,0 +1,31 @@
+export default function DesafioLista(){
+        function metodoArray(){
+            function final(){
+                const a = []
+                for(let i=1 ; i < 11 ; i++ ){
+                a.push(<span>{i}</span>)
+                } 
+            }
+            return <div>{final()}</div>
+        }
+
+        const a = [1,2,3,4,5,6,7,8,10]
+
+        const a2 = ()=>a.map(a => <span>{a+','}</span>)
+        
+       const a3 = ()=> <div>{a2()}</div>
+        
+        
+        return (
+            <div>
+
+            {a3()}
+
+
+            </div>
+    )
+
+
+
+
+}
